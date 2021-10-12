fecde74a44b77473c8bdc3a79f4f7e678b0625e2
尤鈞弘 <48759969+guiguzi19971006@users.noreply.github.com>
2021-10-13 00:01:07 +0800

    更新 Model 結構

---
+++ app/Models/Product.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $table = 'products';
+
+    public function orders() {
+        return $this->belongsToMany(Order::class);
+    }
+
+    public function photos() {
+        return $this->morphMany(Photo::class, 'imageable');
+    }
+
+    public function statuses() {
+        return $this->morphMany(Status::class, 'statusable');
+    }
+}
---
+++ app/Models/User.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+    use HasFactory;
+
+    protected $table = 'users';
+
+    public function orders() {
+        return $this->hasMany(Order::class);
+    }
+
+    public function photos() {
+        return $this->morphMany(Photo::class, 'imageable');
+    }
+
+    public function user_token_types() {
+        return $this->belongsToMany(UserTokenType::class, 'user_tokens', 'user_id', 'user_token_type_id');
+    }
+}
