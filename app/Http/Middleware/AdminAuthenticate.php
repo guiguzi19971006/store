5d9a3f84e89b690e5defdc5b05e4c08d83613dd1
尤鈞弘 <48759969+guiguzi19971006@users.noreply.github.com>
2021-10-31 21:26:32 +0800

    新增管理者登入驗證 Middleware

---
+++ app/Http/Middleware/AdminAuthenticate.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class AdminAuthenticate
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (!$request->session()->has('admin')) {
+            return redirect()->route('admin.login');
+        }
+
+        return $next($request);
+    }
+}
