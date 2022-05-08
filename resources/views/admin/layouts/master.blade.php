437a4be9ff165445d7420d290af22b9cbabfebff
尤鈞弘 <48759969+guiguzi19971006@users.noreply.github.com>
2022-05-08 18:35:54 +0800

    更新頁面 - 主模板頁面

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -25,8 +25,8 @@
         function logout_process()
         {
             // 表單欄位資料
-            var _token = document.querySelector('meta[name="csrf-token"]');
-            var response, xhr = new XMLHttpRequest();
+            let _token = document.querySelector('meta[name="csrf-token"]');
+            let response, xhr = new XMLHttpRequest();
             xhr.open('POST', '/admin/logout_process');
             xhr.onreadystatechange = function () {
                 if (this.readyState == 4 && this.status == 200) {
