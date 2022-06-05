7a1b262ea558264c83e6a83539f938cd4b40630f
尤鈞弘 <48759969+guiguzi19971006@users.noreply.github.com>
2022-06-06 01:51:49 +0800

    更新頁面 - 顯示所有產品頁面

--- resources/views/admin/product/index.blade.php
+++ resources/views/admin/product/index.blade.php
@@ -21,7 +21,7 @@
                     <th>有效日期</th>
                     <th>可否販售</th>
                     <th>建立時間</th>
-                    <th>最後更新時間</th>
+                    <th>更新時間</th>
                 </tr>
             </thead>
 
@@ -51,8 +51,162 @@
         {{ $products->links() }}
 
         <div class="row">
-            <div class="col-12">
+            <div class="col-6">
                 <a href="{{ route('admin.products.create') }}" class="btn btn-primary">新增產品</a>
+            </div>
+
+            <div class="col-6">
+                <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#search-products-modal">
+                    搜尋產品
+                </button>
+            </div>
+        </div>
+    </div>
+
+    <div class="modal" id="search-products-modal">
+        <div class="modal-dialog modal-lg modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title">搜尋產品</h4>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                </div>
+
+                <div class="modal-body">
+                    <form action="{{ route('admin.products.search') }}" method="get" id="search-products-form">
+                        <div class="row">
+                            <div class="col-6">
+                                關鍵字
+                            </div>
+
+                            <div class="col-6">
+                                <input type="text" id="keyword" name="keyword">
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-3">
+                                最低價格
+                            </div>
+
+                            <div class="col-3">
+                                <input type="number" id="lowest-price" name="price[]">
+                            </div>
+
+                            <div class="col-3">
+                                最高價格
+                            </div>
+
+                            <div class="col-3">
+                                <input type="number" id="highest-price" name="price[]">
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-3">
+                                最低庫存量
+                            </div>
+
+                            <div class="col-3">
+                                <input type="number" id="lowest-remaining_qty" name="remaining_qty[]">
+                            </div>
+
+                            <div class="col-3">
+                                最高庫存量
+                            </div>
+
+                            <div class="col-3">
+                                <input type="number" id="highest-remaining_qty" name="remaining_qty[]">
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-3">
+                                開始製造日期
+                            </div>
+
+                            <div class="col-3">
+                                <input type="date" id="start-manufacture_date" name="manufacture_date[]">
+                            </div>
+
+                            <div class="col-3">
+                                結束製造日期
+                            </div>
+
+                            <div class="col-3">
+                                <input type="date" id="end-manufacture_date" name="manufacture_date[]">
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-3">
+                                開始有效日期
+                            </div>
+
+                            <div class="col-3">
+                                <input type="date" id="start-expiration_date" name="expiration_date[]">
+                            </div>
+
+                            <div class="col-3">
+                                結束有效日期
+                            </div>
+
+                            <div class="col-3">
+                                <input type="date" id="end-expiration_date" name="expiration_date[]">
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-6">
+                                可否販售
+                            </div>
+
+                            <div class="col-6">
+                                <select name="is_sellable" id="is_sellable">
+                                    <option value="Y">是</option>
+                                    <option value="N">否</option>
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-3">
+                                最初建立時間
+                            </div>
+
+                            <div class="col-3">
+                                <input type="datetime-local" id="first-created_at-time" name="created_at[]">
+                            </div>
+
+                            <div class="col-3">
+                                最後建立時間
+                            </div>
+
+                            <div class="col-3">
+                                <input type="datetime-local" id="last-created_at-time" name="created_at[]">
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-3">
+                                最初更新時間
+                            </div>
+
+                            <div class="col-3">
+                                <input type="datetime-local" id="first-updated_at-time" name="updated_at[]">
+                            </div>
+
+                            <div class="col-3">
+                                最後更新時間
+                            </div>
+
+                            <div class="col-3">
+                                <input type="datetime-local" id="last-updated_at-time" name="updated_at[]">
+                            </div>
+                        </div>
+
+                        <button type="submit" class="btn btn-dark">搜尋產品</button>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
